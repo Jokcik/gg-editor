a4db4bf6b370b260aaa2c7b2e549b9ab69e2f9c1
Alexey Gerasimov <eddiebrock13@gmail.com>
2018-06-15 17:22:33 +0500

    toggler

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
   public toggler: false;
+  public togglerSpoiler: false;
   constructor() { }
 
   ngOnInit() {
